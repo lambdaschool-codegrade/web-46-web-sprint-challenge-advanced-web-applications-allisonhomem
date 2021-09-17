ba54c7b4a4dd93f95bf8ffa48a9c1d81fbfcbab4
Allison Homem <mrs.allisonhomem@outlook.com>
2021-09-17 13:25:32 -0500

    finished bubblepage tests and answered interview questions

--- interview_answers.md
+++ interview_answers.md
@@ -4,8 +4,16 @@ Be prepared to demonstrate your understanding of this week's concepts by answeri
 
 1. Explain what a token is used for.
 
+a token is used to mark users with a valid login and allow them to access private routes.
+
 2. What steps can you take in your web apps to keep your data secure?
+
+create private or protected routes that users need a token to access.
 
 3. Describe how web servers work.
 
+a web server is a software program that fetches the project code when a user requests it, usually by visiting the associated url and interacting (such as logins).
+
 4. Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+
+create(axios.post), read(axios.get), update(axios.put) and delete(axios.delete)
--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -3,11 +3,30 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
+import mockFetchColorService from '../services/fetchColorService.js';
+jest.mock('../services/fetchColorService.js')
 
-test("Renders without errors", ()=> {
+const twoColors = [  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4",
+    },
+    id: 4,
+  }];
+
+test("Renders appropriate number of colors passed in through mock", async() => {
+    mockFetchColorService.mockResolvedValueOnce({data: twoColors});
     
-});
+    render(<BubblePage/>);
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+    const colors = await screen.findAllByTestId('color');
+
+    expect(colors).toHaveLength(2);
 });
